76d54f0ca7d982e6667261d39e2edeedb0a01863
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2015-03-14 17:23:21 +0000

    Refactor dragrigidbody

--- Assets/Standard Assets/Scripts/General Scripts/DragRigidbody.js
+++ Assets/Standard Assets/Scripts/General Scripts/DragRigidbody.js
@@ -3,68 +3,67 @@ var damper = 5.0;
 var drag = 10.0;
 var angularDrag = 5.0;
 var distance = 0.2;
-var attachToCenterOfMass = false;
 
 private var springJoint : SpringJoint;
 
 function Update ()
 {
 	// Make sure the user pressed the mouse down
-	if (!Input.GetMouseButtonDown (0))
-		return;
-
-	var mainCamera = FindCamera();
+	if (Input.GetMouseButtonDown (0)){
 		
-	// We need to actually hit an object
-	var hit : RaycastHit;
-	if (!Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),  hit, 100))
-		return;
-	// We need to hit a rigidbody that is not kinematic
-	if (!hit.rigidbody || hit.rigidbody.isKinematic)
-		return;
-	
-	if (!springJoint)
-	{
-		var go = new GameObject("Rigidbody dragger");
-		var body : Rigidbody = go.AddComponent.<Rigidbody>() as Rigidbody;
-		springJoint = go.AddComponent.<SpringJoint>();
-		body.isKinematic = true;
-	}
-	
-	springJoint.transform.position = hit.point;
-	if (attachToCenterOfMass)
-	{
-		var anchor = transform.TransformDirection(hit.rigidbody.centerOfMass) + hit.rigidbody.transform.position;
-		anchor = springJoint.transform.InverseTransformPoint(anchor);
-		springJoint.anchor = anchor;
-	}
-	else
-	{
+
+		var mainCamera = FindCamera();
+			
+		// We need to actually hit an object
+		var hit : RaycastHit;
+		if (!Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),  hit, 100))
+			return;
+		// We need to hit a rigidbody that is not kinematic
+		if (!hit.rigidbody || hit.rigidbody.isKinematic)
+			return;
+		
+		if (!springJoint)
+		{
+			var go = new GameObject("Rigidbody dragger");
+			var body : Rigidbody = go.AddComponent.<Rigidbody>() as Rigidbody;
+			springJoint = go.AddComponent.<SpringJoint>();
+			body.isKinematic = true;
+		}
+		
+		springJoint.transform.position = hit.point;
+		
 		springJoint.anchor = Vector3.zero;
+		
+		
+		springJoint.spring = spring;
+		springJoint.damper = damper;
+		springJoint.maxDistance = distance;
+		springJoint.connectedBody = hit.rigidbody;
+		StartCoroutine ("DragObject", hit.distance);
 	}
 	
-	springJoint.spring = spring;
-	springJoint.damper = damper;
-	springJoint.maxDistance = distance;
-	springJoint.connectedBody = hit.rigidbody;
+	if(springJoint && springJoint.connectedBody){
+		DragObject();
+	}
 	
-	StartCoroutine ("DragObject", hit.distance);
+	
 }
 
-function DragObject (distance : float)
+function DragObject ()
 {
 	var oldDrag = springJoint.connectedBody.drag;
 	var oldAngularDrag = springJoint.connectedBody.angularDrag;
 	springJoint.connectedBody.drag = drag;
 	springJoint.connectedBody.angularDrag = angularDrag;
 	var mainCamera = FindCamera();
-	while (Input.GetMouseButton (0))
-	{
-		var ray = GetWorldPositionOnPlane (Input.mousePosition, 0);
-		springJoint.transform.position = ray;//new Vector3(ray.x,0,ray.y); //ray.GetPoint(distance);
-		yield;
-	}
-	if (springJoint.connectedBody)
+	
+	var ray = GetWorldPositionOnPlane (Input.mousePosition, 0);
+	springJoint.transform.position = ray;//new Vector3(ray.x,0,ray.y); //ray.GetPoint(distance);
+		
+		
+		
+	
+	if (springJoint.connectedBody && Input.GetMouseButtonUp(0)) 
 	{
 		springJoint.connectedBody.drag = oldDrag;
 		springJoint.connectedBody.angularDrag = oldAngularDrag;
