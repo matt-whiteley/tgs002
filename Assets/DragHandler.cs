8e2157503a4f4f0e834d21e2485b1c91b719b580
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2015-03-15 16:59:14 +0000

    snap to grid smoothly on drag

--- Assets/DragHandler.cs
+++ Assets/DragHandler.cs
@@ -7,6 +7,13 @@ public class DragHandler : MonoBehaviour {
 
 	public bool atLeastOneFrame = false;
 
+	private Vector3 currentTarget;
+	private float perc = 0;
+
+	void Start(){
+		currentTarget = this.transform.position;
+	}
+
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
@@ -22,7 +29,7 @@ public class DragHandler : MonoBehaviour {
 		}
 
 		if(tile){
-			Debug.Log("about to drag");
+			//Debug.Log("about to drag");
 
 			atLeastOneFrame = DragObject();
 		}
@@ -31,7 +38,20 @@ public class DragHandler : MonoBehaviour {
 
 	bool DragObject(){
 		Vector3 ray = GetWorldPositionOnPlane (Input.mousePosition, 0);
-		tile.transform.position = ray;
+
+
+		Vector3 newTarget = ClampToGrid (ray);
+
+		// lerps the tile to the ocation the pinter is in, unless the target chanegs then reset lerp
+
+		if (currentTarget == newTarget) {
+			perc += Time.deltaTime * 5f;
+		} else {
+			currentTarget = newTarget;
+			perc = 0f;
+		}
+
+		tile.transform.position = Vector3.Lerp (tile.transform.position, ClampToGrid(ray), perc);
 
 		if (Input.GetMouseButtonDown(0) && atLeastOneFrame) {
 			tile = null;			
@@ -49,5 +69,9 @@ public class DragHandler : MonoBehaviour {
 		xy.Raycast(ray, out distance); // intersect ray with plane - distance is length along ray of point of intersection
 		
 		return ray.GetPoint(distance); // return 3D coordinates at 'distance' along ray
+	}
+
+	Vector3 ClampToGrid(Vector3 point){
+		return new Vector3 (Mathf.Round (point.x), 0.0f, Mathf.Round (point.z));
 	}
 }
