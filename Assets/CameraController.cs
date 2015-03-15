2b2e83e9a352092b36b4908b87f457bb6bf91acd
Daniel Parr <danparr@Daniels-MacBook-Pro.local>
2015-03-15 17:50:53 +0000

    camera movement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float speed = 0.0005f;
+
+
+	void Update () {
+
+		float newX = transform.position.x + Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		float newZ = transform.position.z + Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+
+		transform.position = new Vector3 (newX, transform.position.y, newZ);
+
+
+	}
+}
